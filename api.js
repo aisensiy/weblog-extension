dcd1146697fc5043d6a39816615735b06adada4b
aisensiy <aisensiy@gmail.com>
2012-12-31 21:26:37 +0800

    add api

--- api.js
+++ api.js
@@ -1,9 +1,10 @@
 (function() {
     var url_prefix = '';
     function upload_data(tabid, obj) {
-        $.post('/api/uploaddata', obj, function() {
-            cmd.clear(tabid);
-        });
+        // $.post('/api/uploaddata', obj, function() {
+        //     cmd.clear(tabid);
+        // });
+        console.log("duration: " + (new Date() - obj.start_at));
     }
 
 })();
